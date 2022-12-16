acbc5d06995fe32239bc4b8d6ad401c061e925d0
Aadesh Bhardwaj <bhardwajaadesh0001@gmail.com>
2022-12-16 23:32:44 +0530

    AMI shell script final update

--- Packer/NodejS_CWA_CDA.sh
+++ Packer/NodejS_CWA_CDA.sh
@@ -7,7 +7,15 @@ sudo apt install gcc make -y
 sudo apt install nodejs -y
 sudo npm install pm2@latest -g
 
-# AmazonCloudWatch Agent
+#AmazonCodeDeploy Agent
+sudo apt update -y
+sudo apt install ruby wget -y
+cd /home/ubuntu
+wget https://aws-codedeploy-ca-central-1.s3.ca-central-1.amazonaws.com/latest/install
+sudo chmod +x ./install
+sudo ./install auto
+
+#AmazonCloudWatch Agent
 sudo apt update -y
 sudo mkdir /tmp/cwa
 cd /tmp/cwa
@@ -17,16 +25,7 @@ sudo unzip -o AmazonCloudWatchAgent.zip
 sudo ./install.sh
 # sudo mkdir -p /usr/share/collectd/
 # sudo touch /usr/share/collectd/types.db 
-sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -s -c ssm:
-sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -m ec2 -a status
-systemctl start amazon-cloudwatch-agent.service
-systemctl enable amazon-cloudwatch-agent.service
+# sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -c file:/opt/aws/amazon-cloudwatch-agent/bin/config.json -s
+sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -s -c ssm:AmazonCloudWatch-linux-Aadesh
+# /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -m ec2 -a status
 systemctl status amazon-cloudwatch-agent.service
-
-#AmazonCodeDeploy Agent
-sudo apt update -y
-sudo apt install ruby wget -y
-cd /home/ubuntu
-wget https://aws-codedeploy-ca-central-1.s3.ca-central-1.amazonaws.com/latest/install
-sudo chmod +x ./install
-sudo ./install auto
