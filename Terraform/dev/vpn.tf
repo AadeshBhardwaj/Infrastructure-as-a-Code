32dcc9c765d4cf7d1ca1d7ffea905e8d2f0f4c08
Aadesh Bhardwaj <bhardwajaadesh0001@gmail.com>
2022-12-19 16:13:06 +0530

    last commit

--- Terraform/dev/vpn.tf
+++ Terraform/dev/vpn.tf
@@ -25,21 +25,18 @@ resource "aws_security_group" "pritunl-sg" {
   vpc_id = module.vpc.vpc_id
 
   ingress {
-    description = "TLS from VPC"
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
   ingress {
-    description = "TLS from VPC"
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
   ingress {
-    description = "TLS from VPC"
     from_port   = 443
     to_port     = 443
     protocol    = "tcp"
